6d4fc124ec6eed7d785ed13290aea3faa3034ec2
Dauren <makuldauren@gmail.com>
2016-07-26 19:26:12 +0600

    Qibla Added

---
+++ PrayApp/PRGeoPointCompass.swift
@@ -0,0 +1,13 @@
+//
+//  PRGeoPointCompass.swift
+//  PrayApp
+//
+//  Created by Даурен Макул on 26.07.16.
+//  Copyright © 2016 Dauren. All rights reserved.
+//
+
+import UIKit
+
+class PRGeoPointCompass: NSObject {
+
+}
