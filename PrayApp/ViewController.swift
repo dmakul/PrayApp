98dba154323c1029e74131d117a2a11c87591c5f
Dauren <makuldauren@gmail.com>
2016-07-26 14:57:30 +0600

    Initial Commit

---
+++ PrayApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrayApp
+//
+//  Created by Даурен Макул on 26.07.16.
+//  Copyright © 2016 Dauren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
